1f06b52676f3e92340751cf854ee9db6125e12ab
Erwin Caluag <erwin.caluag@digipay.ph>
2022-11-18 13:24:45 +0800

    Changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <section className="footer w-full h-[70px] bg-[#0a192f] py-[20px]">
       <div className="footer--wrapper flex justify-center items-center text-white flex flex-col">
         <p className="text-xs">© 2022 Copyright | All Rights Reserved</p>
-        <p className="text-xs"> Made by Erwin James Caluag</p>
+        <p className="text-xs"> Made by Erwin James </p>
       </div>
     </section>
   );
